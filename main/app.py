c53db95b870ce187155e3c4152ddd8abcb6dc97c
Dheer <dheerm309@gmail.com>
2022-07-31 18:46:19 +0530

    make models

--- main/app.py
+++ main/app.py
@@ -1,4 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Initiate Flask app
 app = Flask(__name__)
+
+# App configurations
+app.config['SECRET_KEY'] = '96cdd36bb88ee75a099543e467d621c4' # Random secret key for protecting from CSRF attacks
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dataistics.db' # SQLite database
+
+# Initiate database
+db = SQLAlchemy(app)
+
+from main import routes
