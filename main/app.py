c3a91eac7c2edb7c00ecf269f3c3828267b926a5
pritansh sahsani <82302289+pritansh-sahsani@users.noreply.github.com>
2022-07-31 14:24:15 +0530

    account system fixed

--- main/app.py
+++ main/app.py
@@ -35,16 +35,16 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password")
 
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
