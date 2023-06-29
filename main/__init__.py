47f612f6ce20186f87f5052ca26ce26865955432
dheer309 <dheerm309@gmail.com>
2023-06-29 19:55:00 +0530

    added admin and logout

--- main/__init__.py
+++ main/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from main.config import Config
 from flask_mail import Mail
 
@@ -11,9 +13,20 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 mail = Mail(app)
 bcrypt = Bcrypt(app)
+admin = Admin(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+from main.models import *
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Posts, db.session))
+admin.add_view(ModelView(Comment, db.session))
+admin.add_view(ModelView(Subscribers, db.session))
+admin.add_view(ModelView(Likes, db.session))
+admin.add_view(ModelView(Messages, db.session))
+admin.add_view(ModelView(MessageReply, db.session))
 
 from main import routes
--- main/routes.py
+++ main/routes.py
@@ -323,3 +323,8 @@ def login():
             flash('Login unsuccessful, please check your email and password', 'danger')
 
     return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
