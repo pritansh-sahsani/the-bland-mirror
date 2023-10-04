d00806ad9983b185959162aa1b4f6fd259335a77
dheer309 <dheerm309@gmail.com>
2023-10-04 12:49:34 +0530

    added post views per day graph

--- main/dash_application/__init__.py
+++ main/dash_application/__init__.py
@@ -3,35 +3,44 @@ from dash import dcc, html
 from flask_login.utils import login_required
 import plotly.express as px
 import pandas as pd
-
-df = pd.DataFrame(
-    {
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-    }
-)
+from main.models import PageViews
 
 
 def create_dash_application(flask_app):
     dash_app = dash.Dash(server=flask_app, name="Dashboard", url_base_pathname="/dash/")
     dash_app._favicon = ("logo.png")
 
-    dash_app.layout = html.Div(
-        children=[
-            html.H1(children="Hello Dash", className="h1"),
-            html.Div(
-                children="""
-            Dash: A web application framework for Python.
-        """
-            ),
-            dcc.Graph(
-                id="example-graph",
-                className="container",
-                figure=px.bar(df, x="Fruit", y="Amount", color="City", barmode="group"),
-            ),
-        ]
-    )
+    # Code for the UI
+    def app_layout():
+        pageviews = PageViews.query.order_by(PageViews.date.desc()).all()
+
+        dates = [pageview.date for pageview in pageviews]
+        count = [pageview.count for pageview in pageviews]
+
+        pageviewdf = pd.DataFrame(
+            {
+                "Dates": dates,
+                "Count": count,
+            }
+        )
+
+        print("\nDF: \n", pageviewdf, "\n\n\n")
+        return html.Div(
+            className="container mt-4",
+            children=[
+                html.H1(children="Hello Dash", className="h1"),
+                html.Div(
+                    children="""
+                Dash: A web application framework for Python.
+            """
+                ),
+                html.Hr(className="mt-2"),
+                dcc.Graph(id="page-views-graph", figure=px.bar(pageviewdf, x="Dates", y="Count", title="Post Views Per Day")),
+            ]
+        )
+        
+    # Define the layout of the Dash application
+    dash_app.layout = app_layout
 
     for view_function in dash_app.server.view_functions:
         if view_function.startswith(dash_app.config.url_base_pathname):
